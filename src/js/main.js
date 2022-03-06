664ab52004ba560b605a72de755083be52f17b55
zvizvi <5633910@gmail.com>
2022-03-07 01:49:24 +0200

    Options page

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
+/* global chrome */
+
 const platform = location.host.split('.')[0];
 const ideWebsitesList = [
   {
     title: 'GitHub Dev',
+    name: 'gitHubDev',
     baseurl: 'https://github.dev',
     platforms: ['github'],
     type: 'url',
@@ -10,6 +13,7 @@ const ideWebsitesList = [
   },
   {
     title: 'CodeSandbox',
+    name: 'codeSandbox',
     baseurl: `https://codesandbox.io/s/${platform}`,
     platforms: ['github'],
     type: 'url',
@@ -18,6 +22,7 @@ const ideWebsitesList = [
   },
   {
     title: 'GitHub1s',
+    name: 'gitHub1s',
     baseurl: 'https://github1s.com',
     platforms: ['github'],
     type: 'url',
@@ -26,6 +31,7 @@ const ideWebsitesList = [
   },
   {
     title: 'GitlabSurf',
+    name: 'gitlabSurf',
     baseurl: 'https://gitlab.surf',
     platforms: ['gitlab'],
     type: 'repoName',
@@ -34,6 +40,7 @@ const ideWebsitesList = [
   },
   {
     title: 'Repl.it',
+    name: 'replit',
     baseurl: `https://repl.it/${platform}`,
     platforms: ['github'],
     type: 'repoName',
@@ -42,6 +49,7 @@ const ideWebsitesList = [
   },
   {
     title: 'StackBlitz',
+    name: 'stackBlitz',
     baseurl: 'https://githubblitz.com',
     platforms: ['github'],
     type: 'repoName',
@@ -50,6 +58,7 @@ const ideWebsitesList = [
   },
   {
     title: 'Gitpod',
+    name: 'gitpod',
     baseurl: `https://gitpod.io/#https://${platform}.com`,
     platforms: ['github', 'gitlab'],
     type: 'url',
@@ -58,6 +67,7 @@ const ideWebsitesList = [
   },
   {
     title: 'Glitch',
+    name: 'glitch',
     baseurl: 'https://glitch.com/edit/#!/import/github',
     platforms: ['github'],
     type: 'repoName',
@@ -66,6 +76,7 @@ const ideWebsitesList = [
   },
   {
     title: 'Sourcegraph',
+    name: 'sourcegraph',
     baseurl: `https://sourcegraph.com/${platform}.com`,
     platforms: ['github', 'gitlab'],
     type: 'repoName',
@@ -74,14 +85,16 @@ const ideWebsitesList = [
   },
   {
     title: 'Clone in VSCode',
+    name: 'cloneInVSCode',
     baseurl: `vscode://vscode.git/clone?url=https://${platform}.com`,
     platforms: ['github'],
-    type: 'url',
+    type: 'repoName',
     icon: '<svg width="16" height="16" viewBox="0 0 276 276" class="mr-2" style="vertical-align:sub"><g><path d="M246.94 27.6383L194.193 2.24138C188.088 -0.698302 180.791 0.541721 175.999 5.33332L3.32371 162.773C-1.32082 167.008 -1.31548 174.32 3.33523 178.548L17.4399 191.37C21.2421 194.827 26.9682 195.081 31.0619 191.976L239.003 34.2269C245.979 28.9347 255.999 33.9103 255.999 42.6667V42.0543C255.999 35.9078 252.478 30.3047 246.94 27.6383Z" fill="var(--color-fg-default)"/><g><path d="M246.94 228.362L194.193 253.759C188.088 256.698 180.791 255.458 175.999 250.667L3.32371 93.2272C-1.32082 88.9925 -1.31548 81.6802 3.33523 77.4523L17.4399 64.6298C21.2421 61.1733 26.9682 60.9188 31.0619 64.0245L239.003 221.773C245.979 227.065 255.999 222.09 255.999 213.333V213.946C255.999 220.092 252.478 225.695 246.94 228.362Z" fill="var(--color-fg-default)"/></g><g><path d="M194.196 253.763C188.089 256.7 180.792 255.459 176 250.667C181.904 256.571 192 252.389 192 244.039V11.9606C192 3.61057 181.904 -0.571175 176 5.33321C180.792 0.541166 188.089 -0.700607 194.196 2.23648L246.934 27.5985C252.476 30.2635 256 35.8686 256 42.0178V213.983C256 220.132 252.476 225.737 246.934 228.402L194.196 253.763Z" fill="var(--color-fg-default)"/></g></g></svg>',
-    class: 'd-none d-md-block border-top'
+    class: 'd-none d-md-block'
   },
   {
     title: 'Open in VSCode',
+    name: 'openInVSCode',
     platforms: ['github'],
     baseurl: 'https://open.vscode.dev',
     type: 'repoName',
@@ -95,27 +108,44 @@ const repoUrlPath = '/' + location.pathname.split('/').slice(1, 3).join('/');
 const repoUrlFullPath = location.pathname.replace(/\/tree\/master$/, '');
 const hasPackageJson = [...document.querySelectorAll('.Details > .js-active-navigation-container > .Box-row a.js-navigation-open')].some((el) => el.innerText === 'package.json');
 
-switch (platform) {
-  case 'github':
-    addGithubSelectMenu();
-    document.addEventListener('pjax:end', () => {
-      document.getElementById('open-in-web-ide')?.remove();
+let options;
+const init = async function () {
+  const storage = await chrome.storage.sync.get('options');
+  options = storage.options;
+
+  switch (platform) {
+    case 'github': {
+      const itemWithBorderOnTop = ideWebsitesList.find( // first item only
+        (item) => (item.name === 'cloneInVSCode' || item.name === 'openInVSCode') && filterItems(item));
+      if (itemWithBorderOnTop) {
+        itemWithBorderOnTop.class += ' border-top';
+      }
+
       addGithubSelectMenu();
-    });
-    break;
-  case 'gitlab':
-    addGitlabSelectMenu();
-    localStorage.setItem('gl-web-ide-button-selected', 'webide');
-    break;
-  default:
-    break;
-}
+      document.addEventListener('pjax:end', () => {
+        document.getElementById('open-in-web-ide')?.remove();
+        addGithubSelectMenu();
+      });
+      break;
+    }
+    case 'gitlab': {
+      addGitlabSelectMenu();
+      localStorage.setItem('gl-web-ide-button-selected', 'webide');
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+
+init();
 
 function filterItems (item) {
   if (item.title === 'StackBlitz' && !hasPackageJson) {
     return false;
   }
-  return item.platforms.includes(platform);
+  return (!options || options[item.name]) && item.platforms.includes(platform);
 }
 
 function addGithubSelectMenu () {
@@ -134,7 +164,7 @@ function addGithubSelectMenu () {
   <div>
     <ul class="dropdown-menu dropdown-menu-sw">
       ${ideWebsitesList.filter(filterItems).map((item) =>
-    `<li class="${item.class || ''}" data-toggle-for="open-in-web-ide"><a href="${item.baseurl}${item.type === 'repoName' ? repoUrlPath : repoUrlFullPath}" class="dropdown-item" ${item.openInNewTab ? 'target="_blank"' : ''} rel="noopener noreferrer">
+    `<li class="${item.class || ''}" data-toggle-for="open-in-web-ide"><a href="${item.baseurl}${item.type === 'repoName' ? repoUrlPath : repoUrlFullPath}" class="dropdown-item" ${(!options || options.openInNewTab) && item.openInNewTab ? 'target="_blank"' : ''} rel="noopener noreferrer">
         ${item.icon}
         ${item.title}
       </a></li>`).join('')}
---
+++ src/js/options.js
@@ -0,0 +1,89 @@
+/* global chrome */
+
+const defaultOptions = {
+  gitHubDev: true,
+  codeSandbox: true,
+  gitHub1s: true,
+  replit: true,
+  stackBlitz: true,
+  gitpod: true,
+  glitch: true,
+  sourcegraph: true,
+  gitlabSurf: true,
+  openInVSCode: true,
+  cloneInVSCode: true,
+  openInNewTab: true
+};
+let options = { ...defaultOptions };
+
+// Load options from storage
+const load = async function () {
+  const storage = await chrome.storage.sync.get('options');
+  options = storage.options || defaultOptions;
+  show();
+};
+
+// Save options to storage
+const save = async function (object = options) {
+  if (!Object.keys(object).some((key) => key !== 'openInNewTab' && options[key])) {
+    object = {
+      gitHubDev: true,
+      openInNewTab: object.openInNewTab
+    };
+  }
+
+  await chrome.storage.sync.set({
+    options: object
+  });
+  load();
+};
+
+// Show options
+const show = function () {
+  for (const key in defaultOptions) {
+    switch (typeof defaultOptions[key]) {
+      case ('boolean'): {
+        document.getElementById(key).checked = options[key];
+        break;
+      }
+      case ('string'): {
+        document.getElementById(key).value = options[key];
+        break;
+      }
+    }
+  }
+};
+
+// Reset to defaults
+const reset = async function () {
+  await save(defaultOptions);
+};
+
+// On save button clicked
+document.addEventListener('click', async (event) => {
+  if (event.target.id === 'save-button') {
+    for (const key in defaultOptions) {
+      switch (typeof defaultOptions[key]) {
+        case ('boolean'): {
+          options[key] = !!document.getElementById(key).checked;
+          break;
+        }
+        case ('string'): {
+          options[key] = document.getElementById(key).value;
+          break;
+        }
+      }
+    }
+
+    await save(options);
+  }
+});
+
+// On reset button clicked
+document.addEventListener('click', async (event) => {
+  if (event.target.id === 'reset-button') {
+    await reset();
+  }
+});
+
+load();
