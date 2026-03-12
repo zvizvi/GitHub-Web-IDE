a40e997c1f103de6a0d5e8487850a8a603ca97cf
candacemarieh <candacemarieh@gmail.com>
2026-03-12 21:22:35 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05";  or= "unstable";
+  channel = "stable-24.05"; # or= "unstable";
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
